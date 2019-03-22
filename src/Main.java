5f51a405d82a0e85380fbae0e4561c6b5ff464ec
maxin <821034226@qq.com>
2019-03-22 14:55:53 +0800

    twoTest

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,12 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
 
     }
 }
